d361f74cc96eebc654cf3612916c27a12c186749
abby-pfingsten <abbypfin@yahoo.com>
2024-07-25 16:32:57 -0400

    added defualt header to app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import winery from '../images/winery-stock.jpg';
+import Header from '../components/Header';
+
 export default function Home() {
-  return <>landing pages</>;
+  const defaultTheme = createTheme();
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <Header />
+    </ThemeProvider>
+  );
 }
