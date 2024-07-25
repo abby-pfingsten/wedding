76cc28b79fe69efc37a3462dc50e5c43574ad8e6
abby-pfingsten <abbypfin@yahoo.com>
2024-07-25 11:32:24 -0400

    have name text input and button

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,18 +3,12 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-
-import { green, purple } from '@mui/material/colors';
-
 import winery from '../images/winery-stock.jpg';
 
 // const defaultTheme = createTheme();
@@ -22,10 +16,10 @@ import winery from '../images/winery-stock.jpg';
 const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: purple[500],
+      main: '#bca0d3',
     },
     secondary: {
-      main: green[500],
+      main: '#16a085',
     },
   },
 });
@@ -40,6 +34,7 @@ const BackgroundDiv = styled('div')({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
+  filter: 'saturate(30%)',
 });
 
 export default function SignIn() {
@@ -56,73 +51,40 @@ export default function SignIn() {
     <ThemeProvider theme={defaultTheme}>
       <BackgroundDiv>
         <Container component='main' maxWidth='xs'>
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component='h1' variant='h5'>
-              Sign in
-            </Typography>
-            <Box
-              component='form'
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
+          <CssBaseline>
+            <Grid
+              container
+              justifyContent='center'
+              alignItems='center'
+              spacing={2}
             >
-              <TextField
-                margin='normal'
-                required
-                fullWidth
-                id='email'
-                label='Email Address'
-                name='email'
-                autoComplete='email'
-                autoFocus
-              />
-              <TextField
-                margin='normal'
-                required
-                fullWidth
-                name='password'
-                label='Password'
-                type='password'
-                id='password'
-                autoComplete='current-password'
-              />
-              <FormControlLabel
-                control={<Checkbox value='remember' color='primary' />}
-                label='Remember me'
-              />
-              <Button
-                type='submit'
-                fullWidth
-                variant='contained'
-                sx={{ mt: 3, mb: 2 }}
+              <Grid
+                item
+                xs={12}
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
               >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href='#' variant='body2'>
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link href='#' variant='body2'>
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
+                <Typography>Abby and Andrew</Typography>
               </Grid>
-            </Box>
-          </Box>
+              {/* <Stack> */}{' '}
+              <Grid
+                item
+                xs={12}
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+              >
+                <TextField
+                  id='outlined-basic'
+                  label='Please enter the phrase here'
+                  variant='outlined'
+                />{' '}
+              </Grid>
+              <Button variant='contained'>Contained</Button>
+              {/* </Stack> */}
+            </Grid>
+          </CssBaseline>
         </Container>
       </BackgroundDiv>
     </ThemeProvider>
