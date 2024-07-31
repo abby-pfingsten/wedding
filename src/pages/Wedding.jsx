9441eccccbc10920382f4d62627bf0bd42c66164
abby-pfingsten <abbypfin@yahoo.com>
2024-07-31 15:16:56 -0400

    scroll to bottom

--- src/pages/Wedding.jsx
+++ src/pages/Wedding.jsx
@@ -1,10 +1,21 @@
+import { GridView } from '@mui/icons-material';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import * as React from 'react';
 
 export default function Wedding() {
   return (
     <>
-      <Container id='Wedding'>text from the wedding .jsx </Container>
+      <Container id='Wedding'>
+        <Grid
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          asdfasdfasdf
+        </Grid>
+      </Container>
     </>
   );
 }
