857dd0b82563f488fd9d6896cb35f3b47afa3cf8
abby-pfingsten <abbypfin@yahoo.com>
2024-09-19 11:02:16 -0400

    added component to create image for timeline

---
+++ src/components/ImageOurStory.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+export default function ImageOurStory() {
+  ourStoryArray = [
+    {
+      header: 'First Header',
+      subHeader: 'First Sub Header',
+      text: 'First Text',
+      image: 'First Image',
+    },
+    {
+      header: 'Second Header',
+      subHeader: 'Second Sub Header',
+      text: 'Second Text',
+      image: 'Second Image',
+    },
+    {
+      header: 'Third Header',
+      subHeader: 'Third Sub Header',
+      text: 'Third Text',
+      image: 'Third Image',
+    },
+  ];
+
+  return <></>;
+}
