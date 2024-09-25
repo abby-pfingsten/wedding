986c1d5371b5430ddd7704cd4ea0b886749196cf
abby-pfingsten <abbypfin@yahoo.com>
2024-09-25 09:27:47 -0400

    have text in grid, but not using grid v2...grid is deprecated

--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Timeline from '@mui/lab/Timeline';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
@@ -14,27 +15,28 @@ import picThree from '../images/our-story/pic_3.jpg';
 
 export default function OurStory() {
   let ourStoryArray = [];
+
   ourStoryArray = [
     {
       key: 1,
       header: 'First Header',
       subHeader: 'First Sub Header',
       text: 'First Text',
-      image: { picOne },
+      image: picOne,
     },
     {
       key: 2,
       header: 'Second Header',
       subHeader: 'Second Sub Header',
       text: 'Second Text',
-      image: { picTwo },
+      image: picTwo,
     },
     {
       key: 3,
       header: 'Third Header',
       subHeader: 'Third Sub Header',
       text: 'Third Text',
-      image: { picThree },
+      image: picThree,
     },
   ];
 
@@ -48,24 +50,37 @@ export default function OurStory() {
           <TimelineItem key={timelineItem.key}>
             <TimelineSeparator>
               <TimelineConnector sx={{ height: 100 }} />
-              <TimelineDot>
-                <Box
-                  component='img'
-                  sx={{
-                    height: 105,
-                    width: 105,
-                    maxHeight: { xs: 233, md: 167 },
-                    maxWidth: { xs: 350, md: 250 },
-                    borderRadius: 15,
-                    objectFit: 'cover',
-                  }}
-                  // alt='The house from the offer.'
-                  src={timelineItem.image}
-                />
-              </TimelineDot>
+              <Box
+                component='img'
+                sx={{
+                  height: 105,
+                  width: 105,
+                  maxHeight: { xs: 233, md: 167 },
+                  maxWidth: { xs: 350, md: 250 },
+                  borderRadius: 15,
+                  objectFit: 'cover',
+                }}
+                // alt='The house from the offer.'
+                src={timelineItem.image}
+              />
               <TimelineConnector />
             </TimelineSeparator>
-            <TimelineContent>{timelineItem.header}</TimelineContent>
+            <TimelineContent>
+              <Grid container direction='column'>
+                <Grid item xs={2} sm={4} md={4}>
+                  {' '}
+                  {timelineItem.header}
+                </Grid>
+                <Grid item xs={2} sm={4} md={4}>
+                  {' '}
+                  {timelineItem.subHeader}
+                </Grid>
+                <Grid item xs={2} sm={4} md={4}>
+                  {' '}
+                  {timelineItem.text}
+                </Grid>
+              </Grid>
+            </TimelineContent>
           </TimelineItem>
         ))}
       </Timeline>{' '}
