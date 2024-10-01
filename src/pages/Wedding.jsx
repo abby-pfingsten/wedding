90bcb1ecda27b710c39a1585912b76b76ee85548
abby-pfingsten <abbypfin@yahoo.com>
2024-10-01 16:24:43 -0400

    have date subtraction calculator

--- src/pages/Wedding.jsx
+++ src/pages/Wedding.jsx
@@ -1,8 +1,26 @@
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
 import * as React from 'react';
+import { useState } from 'react';
 
 export default function Wedding() {
+  const [timer, setTimer] = useState('00:00:00');
+  // const [currentDate, setCurrentDate] = useState(null);
+
+  const currentDate = new Date();
+  // setCurrentDate(new Date());
+
+  const weddingWeekend = new Date('2026-09-01T16:15:00Z');
+
+  function subtractDates(currentDate, weddingDate) {
+    // setCurrentDate(new Date());
+    const millisecondsDiff = weddingDate.getTime() - currentDate.getTime();
+    const daysDiff = Math.floor(millisecondsDiff / (1000 * 60 * 60 * 24));
+
+    return daysDiff;
+  }
+
   return (
     <>
       <Container id='Wedding'>
@@ -12,6 +30,8 @@ export default function Wedding() {
             flexDirection: 'column',
           }}
         >
+          {/* subtractDates(currentDate, weddingWeekend) */}
+          <Typography>{subtractDates(currentDate, weddingWeekend)}</Typography>
           TEXT TEXT TEXT TEXT TEXT TEXTTEXT TEXT TEXTTEXT TEXT TEXTTEXT TEXT
           TEXTTEXT TEXT TEXTTEXT TEXT TEXTTEXT TEXT TEXTTEXT TEXT TEXTTEXT TEXT
           TEXTTEXT TEXT TEXTTEXT TEXT TEXTTEXT TEXT TEXTTEXT TEXT TEXTTEXT TEXT
