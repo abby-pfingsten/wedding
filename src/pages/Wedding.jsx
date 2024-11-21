d65c9de827171f2de6d33fd3407ad95fe90b2d33
abby-pfingsten <abbypfin@yahoo.com>
2024-11-21 15:08:09 -0500

    have items and important information aligned properly

--- src/pages/Wedding.jsx
+++ src/pages/Wedding.jsx
@@ -8,9 +8,11 @@ import weddingPagePics from '../data/wedding-page-data.jsx';
 import Popover from '../components/Popover.jsx';
 
 export default function Wedding() {
+  // create the two dates
   const currentDate = new Date();
-  const weddingWeekend = new Date('2026-09-01T16:15:00Z');
+  const weddingWeekend = new Date('2026-09-04T16:15:00Z');
 
+  // subtract the current date from the date of when the wedding is
   function subtractDates(currentDate, weddingDate) {
     const millisecondsDiff = weddingDate.getTime() - currentDate.getTime();
     const daysDiff = Math.floor(millisecondsDiff / (1000 * 60 * 60 * 24));
@@ -24,27 +26,36 @@ export default function Wedding() {
           sx={{
             display: 'flex',
             flexDirection: { xs: 'column', sm: 'row', mid: 'row' },
+            justifyContent: 'space-between',
           }}
         >
-          {/* <Item> */}
-          <ImageList
-            sx={{ width: 600, height: 650 }}
-            variant='quilted'
-            cols={6}
-            rowHeight={121}
+          <Grid>
+            <ImageList
+              sx={{ width: 600, height: 650 }}
+              variant='quilted'
+              cols={6}
+              rowHeight={110}
+            >
+              {weddingPagePics.map((item) => (
+                <ImageListItem
+                  key={item.key}
+                  cols={item.cols || 1}
+                  rows={item.rows || 1}
+                >
+                  <img src={item.img} alt={item.title} />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Grid>
+          <Grid
+            offset={7}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-evenly',
+              alignItems: 'center',
+            }}
           >
-            {weddingPagePics.map((item) => (
-              <ImageListItem
-                key={item.key}
-                cols={item.cols || 1}
-                rows={item.rows || 1}
-              >
-                <img src={item.img} alt={item.title} />
-              </ImageListItem>
-            ))}
-          </ImageList>
-          {/* </Item> */}
-          <Grid offset={7}>
             <Typography variant='h2'>Abigale & Andrew</Typography>
             <Typography variant='h5'>
               September 6th, 2026 - September 6th, 2026
