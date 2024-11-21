a20d1c07fe740cc180c98702ba2e840524e83b24
abby-pfingsten <abbypfin@yahoo.com>
2024-11-21 11:31:43 -0500

    put data/pics from home page into sep file

---
+++ src/data/wedding-page-data.jsx
@@ -0,0 +1,34 @@
+import first from '../images/ab-anj-bridge.jpg';
+import second from '../images/ab-anj-meelo.jpg';
+import third from '../images/ab-anj-kiss.jpg';
+import fourth from '../images/ab-anj-hug.jpg';
+
+const itemData = [
+  {
+    key: 1,
+    img: first,
+    title: 'Breakfast',
+    rows: 2,
+    cols: 2,
+  },
+  {
+    key: 2,
+    img: second,
+    title: 'Breakfast',
+    rows: 2,
+    cols: 4,
+  },
+  {
+    key: 3,
+    img: third,
+    title: 'Breakfast',
+    cols: 2,
+  },
+  {
+    key: 4,
+    img: fourth,
+    title: 'Breakfast',
+  },
+];
+
+export default itemData;
--- src/pages/Wedding.jsx
+++ src/pages/Wedding.jsx
@@ -6,10 +6,7 @@ import { useState, useEffect } from 'react';
 import { timelineClasses } from '@mui/lab';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import first from '../images/ab-anj-bridge.jpg';
-import second from '../images/ab-anj-meelo.jpg';
-import third from '../images/ab-anj-kiss.jpg';
-import fourth from '../images/ab-anj-hug.jpg';
+import weddingPageData from '../data/wedding-page-data.jsx';
 
 import { FirstPageOutlined } from '@mui/icons-material';
 
@@ -59,33 +56,6 @@ export default function Wedding() {
   //   console.log(timeLeft);
   // });
 
-  const itemData = [
-    {
-      key: 1,
-      img: first,
-      title: 'Breakfast',
-      rows: 2,
-      cols: 2,
-    },
-    {
-      key: 2,
-      img: second,
-      title: 'Breakfast',
-      rows: 2,
-      cols: 4,
-    },
-    {
-      key: 3,
-      img: third,
-      title: 'Breakfast',
-      cols: 2,
-    },
-    {
-      key: 4,
-      img: fourth,
-      title: 'Breakfast',
-    },
-  ];
   return (
     <>
       <Container id='Wedding'>
@@ -104,7 +74,7 @@ export default function Wedding() {
             cols={4}
             rowHeight={121}
           >
-            {itemData.map((item) => (
+            {weddingPageData.map((item) => (
               <ImageListItem
                 key={item.key}
                 cols={item.cols || 1}
@@ -114,7 +84,6 @@ export default function Wedding() {
                   src={item.img}
                   {...srcset(item.img, 121, item.rows, item.cols)}
                   alt={item.title}
-                  // loading='lazy'
                 />
               </ImageListItem>
             ))}
