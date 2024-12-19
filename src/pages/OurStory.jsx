c0f39117e3373bf1485886e73a5bcbe908419bd4
abby-pfingsten <abbypfin@yahoo.com>
2024-12-19 10:22:19 -0500

    deleted dead code

--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -18,53 +18,6 @@ import '../styles/OurStory.scss';
 import ourStoryPics from '../data/our-story-data.jsx';
 
 export default function OurStory() {
-  // let ourStoryArray = [];
-
-  // ourStoryArray = [
-  //   {
-  //     key: 1,
-  //     header: 'August 2016',
-  //     subHeader: 'Semi-Formal',
-  //     text: 'We first became friends at the beginning of our freshman year, after Abby successfully inserted herself into the Stever 1 crew. Andrew had his first semi-formal in September and Abby was his third choice!',
-  //     image: freshmanFormal,
-  //   },
-  //   {
-  //     key: 2,
-  //     header: 'October/November 2016',
-  //     subHeader: 'Addams Family Performance',
-  //     text: 'This was the first time Andrew saw Abby performing, as Wednesday in The Addams Family. We both had feelings for each other at this point, but we were trying to hide it. Andrew asked Abby to be his girlfriend shortly after this in November.',
-  //     image: freshmanAddams,
-  //   },
-  //   {
-  //     key: 3,
-  //     header: 'November 2017',
-  //     subHeader: 'First Thanksgiving Together',
-  //     text: 'We proceeded to spend the rest of our sophomore...',
-  //     image: sophomoreYear,
-  //   },
-  //   {
-  //     key: 4,
-  //     header: 'May 2018',
-  //     subHeader: 'Speakeasy in Detroit',
-  //     text: 'Junior...',
-  //     image: juniorYear,
-  //   },
-  //   {
-  //     key: 5,
-  //     header: 'November 2019',
-  //     subHeader: 'Abby 21st B-day Dinner',
-  //     text: 'And senior years of college together',
-  //     image: seniorYear,
-  //   },
-  //   {
-  //     key: 6,
-  //     header: 'November 2020',
-  //     subHeader: 'Meelo!',
-  //     text: 'After finishing college in May of 2020, we finally got our own apartment together and adopted the best most special boy in the entire world.',
-  //     image: meelo,
-  //   },
-  // ];
-
   return (
     <Container id='Our Story'>
       <Typography variant='h2' align='center'>
