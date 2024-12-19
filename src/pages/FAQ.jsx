00aa83b07263c07086c0307a921759389ce48d40
abby-pfingsten <abbypfin@yahoo.com>
2024-12-19 10:27:28 -0500

    added faq data

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -5,41 +5,15 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import Typography from '@mui/material/Typography';
+import faqData from '../data/faq-data.jsx';
 
 export default function FAQ() {
-  const faqObject = [
-    {
-      key: 1,
-      faqTitle: 'Accomodations?',
-      faqAnswer: 'hell naw',
-    },
-    {
-      key: 2,
-      faqTitle: 'Food? ',
-      faqAnswer: 'hell ya',
-    },
-    {
-      key: 3,
-      faqTitle: 'When to RSVP By?',
-      faqAnswer: 'yes and he will be looking extra dapper',
-    },
-    {
-      key: 4,
-      faqTitle: 'Can I Bring a Plus One?',
-      faqAnswer: 'Answer 1',
-    },
-    {
-      key: 5,
-      faqTitle: 'Will Mr Meelo Be Attending?',
-      faqAnswer: 'Answer 1',
-    },
-  ];
   return (
     <Container id='Q&A'>
       <Typography variant='h2' align='center' sx={{ marginTop: 20 }}>
         Your Burning Questions Answered
       </Typography>
-      {faqObject.map((faq) => (
+      {faqData.map((faq) => (
         <Accordian key={faq.key}>
           <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
             {faq.faqTitle}
--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
 
 function Schedule() {
   return (
     <>
       <Container id='Schedule'>
-        <Typography varient='h2' align='center'>
-          {' '}
-          Schedule
-        </Typography>
+        <Grid
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-evenly',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <Typography varient='h2' align='center'>
+            {' '}
+            Schedule
+          </Typography>
+        </Grid>
       </Container>
     </>
   );
