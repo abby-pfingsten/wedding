783c24bb636dc462fb99d9f02bf9101517652394
abby-pfingsten <abbypfin@yahoo.com>
2024-12-22 13:56:43 -0500

    styling from scss applied

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -4,8 +4,10 @@ import Grid from '@mui/material/Grid';
 import { Typography } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import { experimentalStyled as styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 import scheduleData from '../data/schedule-data.jsx';
+// import styling
+import '../styles/Schedule.scss';
 
 function Schedule() {
   const Item = styled(Paper)(({ theme }) => ({
@@ -43,30 +45,23 @@ function Schedule() {
           {scheduleData.map((item, index) => (
             <Item elevation={8} key={index} className='schedule'>
               <h4 className='schedule__title'>{item.day}</h4>
-              {/* <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'space-evenly',
-                  height: '100%',
-                }}
-              > */}
               {item.activities.map((activity, index) => (
                 <div
+                  className='schedule__activities'
                   key={index}
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'space-evenly',
-                    // height: '100%',
-                  }}
+                  //   style={{
+                  //     display: 'flex',
+                  //     flexDirection: 'column',
+                  //     justifyContent: 'space-evenly',
+                  //     // height: '100%',
+                  //   }}
                 >
-                  {' '}
-                  <p>{activity.time}</p>
-                  <p>{activity.description}</p>
+                  <p className='schedule__activities--time'>{activity.time}</p>
+                  <p className='schedule__activities--description'>
+                    {activity.description}
+                  </p>
                 </div>
               ))}
-              {/* </div> */}
             </Item>
           ))}
         </Grid>
