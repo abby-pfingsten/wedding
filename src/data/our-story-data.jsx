e0ed09045bce7287e522d9de0cc382a64d595eee
abby-pfingsten <abbypfin@yahoo.com>
2024-12-25 16:13:30 -0500

    added better pictures in timeline

--- src/data/our-story-data.jsx
+++ src/data/our-story-data.jsx
@@ -1,10 +1,16 @@
 // import pictures
 import freshmanFormal from '../images/our-story/freshman_year_formal.JPG';
 import freshmanAddams from '../images/our-story/freshman_year_addams.JPEG';
-import sophomoreYear from '../images/our-story/sophomore_year.JPG';
-import juniorYear from '../images/our-story/junior_year.JPG';
-import seniorYear from '../images/our-story/senior_year.JPG';
+// import sophomoreYear from '../images/our-story/sophomore_year.JPG';
+// import juniorYear from '../images/our-story/junior_year.JPG';
+// import seniorYear from '../images/our-story/senior_year.JPG';
 import meelo from '../images/our-story/meelo_thanksgiving.jpg';
+import house from '../images/our-story/bought_house.JPG';
+import van from '../images/our-story/bought-van.jpg';
+import italy from '../images/our-story/italy.JPG';
+import engagement from '../images/our-story/engagement.jpg';
+import rockies from '../images/our-story/rockies.JPG';
+import mexico from '../images/our-story/mexico.jpg';
 
 const ourStoryArray = [
   {
@@ -21,33 +27,75 @@ const ourStoryArray = [
     text: 'This was the first time Andrew saw Abby performing, as Wednesday in The Addams Family. We both had feelings for each other at this point, but we were trying to hide it. Andrew asked Abby to be his girlfriend shortly after this in November.',
     image: freshmanAddams,
   },
+  // {
+  //   key: 3,
+  //   header: 'November 2017',
+  //   subHeader: 'First Thanksgiving Together',
+  //   text: 'We proceeded to spend the rest of our sophomore...',
+  //   image: sophomoreYear,
+  // },
+  // {
+  //   key: 4,
+  //   header: 'May 2018',
+  //   subHeader: 'Speakeasy in Detroit',
+  //   text: 'Junior...',
+  //   image: juniorYear,
+  // },
+  // {
+  //   key: 5,
+  //   header: 'November 2019',
+  //   subHeader: 'Abby 21st B-day Dinner',
+  //   text: 'And senior years of college together',
+  //   image: seniorYear,
+  // },
+  {
+    key: 2,
+    header: 'November 2020',
+    subHeader: 'Meelo!',
+    text: 'After enduring CMU together and graduating in May of 2020, we finally got our own apartment together and adopted the best most special boy in the entire world. Meelo completed our family.',
+    image: meelo,
+  },
   {
     key: 3,
-    header: 'November 2017',
-    subHeader: 'First Thanksgiving Together',
-    text: 'We proceeded to spend the rest of our sophomore...',
-    image: sophomoreYear,
+    header: 'March 2021',
+    subHeader: 'Bought Our Home!',
+    text: "While we loved our first home on Stanton Ave in Pittsburgh's Highland Park neighborhood, we knew we wanted to be able to own our own place, so in March of this year we bought our first home in Pittsburgh's 'Holla'!",
+    image: house,
   },
   {
     key: 4,
-    header: 'May 2018',
-    subHeader: 'Speakeasy in Detroit',
-    text: 'Junior...',
-    image: juniorYear,
+    header: 'May 2021',
+    subHeader: 'First "Real" Vacation Together',
+    text: "In the summer of 2021, we took our first real vacation together to Valladolid in Mexico! Technically, our first true vacation was to Ocean City Maryland as two broke junior's in college, and while that vacation will always hold an extremely special place in our hearts, our trip to Mexico ignited a passion to travel and explore the world together",
+    image: mexico,
   },
   {
     key: 5,
-    header: 'November 2019',
-    subHeader: 'Abby 21st B-day Dinner',
-    text: 'And senior years of college together',
-    image: seniorYear,
+    header: 'May 2022',
+    subHeader: 'Getting Out in Nature',
+    text: 'Along with our growing passion for international travel, we also got really into the outdoors and started taking up hiking and climbing',
+    image: rockies,
   },
   {
     key: 6,
-    header: 'November 2020',
-    subHeader: 'Meelo!',
-    text: 'After finishing college in May of 2020, we finally got our own apartment together and adopted the best most special boy in the entire world.',
-    image: meelo,
+    header: 'September 2023 - ??',
+    subHeader: 'First, But Not Last European Excurscion',
+    text: "Italy was both of our first time's in Europe and we both fell in love with it! Little did we know we would be having our wedding here. Since Italy, we've been able to visit The Netherlands, France, Croatia, Belgium, Germany, Vienna, Portugal, Finland, Latvia and Estonia as part of our European travels.",
+    image: italy,
+  },
+  {
+    key: 7,
+    header: 'April 2024',
+    subHeader: 'We Bought a Van!',
+    text: "Van life was a bucketlist item we both wanted to be able to accomplish in our lifetime. After searching for a few months, we finally found and bought our van (The Abby Mobile, as my Unle Tom has deemed it) in April. This is us celebrating at a local hot dog joint. We can't wait to continue building it out and get to explore all the national parks. ",
+    image: van,
+  },
+  {
+    key: 8,
+    header: 'July 2024',
+    subHeader: 'Engaged!!!',
+    text: 'In the most Andrew fashion possible, he proposed to Abby at our home on July 13th, 2024. It was truly memorable and even though Abby knew he had the ring since February (and was the ONLY person to know about it, besides Manu), she was very surprised and happy. ',
+    image: engagement,
   },
 ];
 export default ourStoryArray;
