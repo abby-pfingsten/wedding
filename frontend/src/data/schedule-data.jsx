c3920adf302b3a678adf9ab2c44d7e4c1fbb1aa7
abby-pfingsten <abbypfin@yahoo.com>
2026-02-04 20:52:27 -0500

    updated schedule

--- frontend/src/data/schedule-data.jsx
+++ frontend/src/data/schedule-data.jsx
@@ -4,16 +4,16 @@ const schedule = [
     daySummary: 'Welcome Dinner',
     activities: [
       {
-        time: '6pm-9pm:',
-        location: 'Location is TBD',
+        time: '7:30pm-10:30pm:',
+        location: 'Bistro Uzeta',
         description:
           "Welcome dinner and drinks in Catania! We can't wait to see everyone!",
       },
       {
-        time: '9pm:',
-        location: 'Location is TBD',
+        time: '10:30pm:',
+        location: 'Antico Borgo Etneo or Hotel Milo Max',
         description:
-          'After dinner, transportation will be provided to get to the hotel in Etna.',
+          'After dinner, transportation will be provided to get to the hotel in Etna/Milo.',
       },
     ],
   },
@@ -22,25 +22,22 @@ const schedule = [
     daySummary: 'Wedding Day!',
     activities: [
       {
-        time: '9am-11:30am:',
-        location: 'Villa',
-        description: 'Breakfast, provided at the hotel.',
+        time: 'Morning-Afternoon',
+        location: 'Wherever you please!',
+        description:
+          'Breakfast, provided at the hotel. Folks who are not involved in the wedding will have free time before the ceremony. ',
       },
       {
-        time: '12pm:',
-        location: 'Location is TBD',
-        description: 'Lunch at a local winery!',
-      },
-      {
-        time: '2pm-5pm:',
-        location: 'Barone di Villagrande Winery or Villa',
-        description: 'Free time to get ready before the ceremony!',
+        time: '4pm:',
+        location: 'Antico Borgo Etneo',
+        description:
+          'We will have transportation ready to pick up guests staying in Antico Borgo at 4pm as it is about a half hour to get to the venue. Those staying in Milo Max will get picked up by car since it is close to the venue.',
       },
       {
         time: '5pm-1am:',
         location: 'Barone di Villagrande Winery',
         description:
-          'Ceremony and wedding festivities. Exact wedding schedule will be provided at the winery. Transportation will be arranged for guests to get back to their hotel',
+          'Ceremony and wedding festivities. Exact wedding schedule will be provided at the winery. Two transportation options will be arranged for guests to get back to their hotels, 10:30PM or 1AM.',
       },
     ],
   },
@@ -49,43 +46,38 @@ const schedule = [
     daySummary: 'Beach Party Celebration!',
     activities: [
       {
-        time: '9am-11:30am:',
-        location: 'Villa',
+        time: 'Morning',
+        location: 'Antico Borgo Etneo or Hotel Milo Max',
         description: 'Breakfast, provided at the hotel.',
       },
       {
-        time: '12pm-5pm:',
-        location: 'Location is TBD',
+        time: '11am-5pm:',
+        location: 'Afrobar Beach Club, Catania',
         description:
-          'Beach day!! We will rent out a beach club, and spend a few hours celebrating our wedding. Lunch and drinks will be provided. ',
+          'Beach day!! We will rent out space at a beach club, and have lunch provided there. At 5pm we will have transportation either back to your hotel in Etna, or to Catania.',
       },
       {
-        time: '5pm-7pm:',
-        location: 'Barone di Villagrande or Villa',
+        time: '9pm?-12:30am?:',
+        location: 'Afrobar Beach Club',
         description:
-          'Transportation will be provided back to the hotels, where guests will have a chance to decompress and get ready for dinner.',
-      },
-      {
-        time: '7:30pm:',
-        location: 'Location is TBD',
-        description: 'Dinner at a local Trattoria.',
+          'If you have elected to stay in Catania, there will be a Night Party at the club we will be going to. Party on!!',
       },
     ],
   },
   {
     day: 'Sunday, August 2nd',
-    daySummary: 'Farewell Brunch',
+    daySummary: 'Farewell Lunch',
     activities: [
       {
-        time: '9am-11am:',
-        location: 'Barone di Villagrande or Villa',
-        description: 'Checkout from the hotel will be at 11am.',
+        time: 'Morning',
+        location: 'Antico Borgo or Hotel Milo Max',
+        description: 'Breakfast, provided at the hotel. Checkout is at 11am.',
       },
       {
-        time: '11:30am-1:30pm:',
+        time: '11am-2pm:',
         location: 'Location is TBD',
         description:
-          'Farewell brunch. Transportation will be provided at 2pm to Catania. ',
+          'Farewell lunch -- idea is pizza and beers. We will have transportation ready for guests at 11am from their hotels to get into Catania. If you have an earlier flight you will need to provide your own transportation to Catania. We hope you enjoy the rest of your travles!',
       },
     ],
   },
